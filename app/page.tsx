5af21544eaca80b09ff2d2076ca5139aafe1fb66
spino17 <bhavyabhatt17@gmail.com>
2023-05-31 20:23:07 +0530

    laying out basic flow for loading wasm module and running the rust function

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,62 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client"; // This is a client component 👈🏽
+
+import { useEffect, useState } from "react";
+import init, { compile } from "../public/pkg/jarvil_wasm";
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [result, setResult] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>(null);
+  const [inputText, setInputText] = useState<string>("");
+  useEffect(() => {
+    const inner_func = async () => {
+      await init(); // initialize the wasm module
+      setIsLoading(false);
+    };
+    inner_func();
+  }, []);
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+  function handleOnClickRun() {
+    try {
+      const result = compile(inputText);
+      setResult(result);
+      setError(null);
+    } catch (error) {
+      if (typeof error == "string") {
+        setError("Error occured: " + error);
+      } else {
+        console.log(typeof error);
+        console.log(error);
+        setError("Something went wrong!");
+      }
+      setResult(null);
+    }
+  }
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(event.target.value);
+  };
+
+  if (isLoading) {
+    return <div>loading ...</div>;
+  } else {
+    return (
+      <div>
+        <input
+          type="text"
+          value={inputText}
+          onChange={handleInputChange}
+          style={{ border: "1px solid #ccc", padding: "10px" }}
         />
+        <div
+          onClick={() => {
+            handleOnClickRun();
+          }}
+        >
+          Run
+        </div>
+        {result ? result : error}
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    );
+  }
 }
